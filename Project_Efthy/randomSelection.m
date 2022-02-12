51aa5168964c70a4a6c7aedde023ac3b9af5c99c
Papalight <theo.papafotiou@gmail.com>
2022-02-12 17:00:20 +0100

    Delete Efthy project after correction

--- Project_Efthy/randomSelection.m
+++
@@ -1,7 +0,0 @@
-function [selectedPopulation] = randomSelection(population, numofSelections)
-
-    y = randperm(height(population), numofSelections);
-    
-    selectedPopulation = population(y, :);
-
-end
